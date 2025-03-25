1f38b2c4f483dfce1cd09b4208a0c62e557966c1
Alaa Abdelhady Zaky <113461454+Alaa2001773@users.noreply.github.com>
2025-03-26 01:02:43 +0200

    Add files via upload

---
+++ EMS/EMS/Department.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS
+{
+    internal class Department
+    {
+
+        public string Name { get; set; }
+        private List<Employee> employees;
+        public Department(string name)
+        {
+                Name = name;
+            employees = new List<Employee>();
+            
+        }
+        public void AddEmployee(Employee employee) 
+        {
+        employees.Add(employee);
+        }    
+        public void RemoveEmployee(Employee employee) 
+        {
+        employees.Remove(employee);
+        }
+    }
+}
---
+++ EMS/EMS/Employee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS
+{
+    internal class Employee
+    {
+        public int Id { get;private set; }
+        private static int nextId = 1;
+        public string Name { get;private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+        public Department Department { get; private set; }
+        public DateTime EmployeeDate { get; private set; }
+        public bool IsTerminated { get; private set; }
+        public int PerformanceRating { get; private set; }
+
+        public Employee(string name, int age, decimal salary, Department department  )
+        {
+            Id = nextId++;
+            Name = name;
+            Age = age;
+            Salary = salary;
+            Department = department;
+            EmployeeDate = DateTime.Now;
+            PerformanceRating = 0;
+            department.AddEmployee(this);
+
+
+
+        }
+        public void Terminate()
+        {
+            IsTerminated = true;
+            Department.RemoveEmployee(this);
+            Console.WriteLine($"Employee {Name} has been terminated.");
+        }
+        public void Promote(string newTitle, decimal increaseAmount)
+        {
+            if (IsTerminated)
+            {
+                Console.WriteLine($"Cannot promote terminated employee {Name}.");
+               
+            }
+
+            Salary += increaseAmount;
+            Console.WriteLine($"Employee {Name} promoted with salary increase: {increaseAmount:F}.");
+        }
+        public void UpdatePerformanceRating(int rating)
+        {
+            if (rating < 1 || rating > 5)
+            {
+                Console.WriteLine("Rating must be between 1 and 5.");
+                return;
+            }
+
+            PerformanceRating = rating;
+            Console.WriteLine($"Performance rating for {Name} updated to {rating}.");
+        }
+
+
+    }
+}
